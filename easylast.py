a77417816e65a613a953bb069e0547916c446a07
yosholo <mathieu.lgrand@gmail.com>
2013-02-08 22:39:08 +0100

    add doc in the module

--- easylast.py
+++ easylast.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import socket
-import lxml
 import re
 from lxml import etree
 
@@ -11,30 +11,34 @@ re_nseason_nep = "S([0-9]+)E([0-9]+)|([0-9]+)x([0-9]+)"
 parserHTML =  etree.HTMLParser( recover=True,encoding='utf-8')
 
 def connect_remote():
+    """ Return a socket connect to host_remote on port_remote """
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((host_remote,port_remote))
 
     return sock
 
 def choose_bd(bd):
-    
+    """ Return a string who determine the argument in the command line
+    the argument is DL or VU """
     arg = b" "    
     if bd == "DL":
         arg = b"-d"
     return arg
 
 def find_real_name_in_bd(name,infos):
-   
+
+    """ Return the infos which correspond to the real name in the bd """    
     ret = None
     for info in infos:
         if re.search(name,info[0],re.IGNORECASE) != None :
             ret = info
     return ret
-    
+
+
 def read_last_xml(bd):
 
+    """ Return the xml send by last_seen, the argument is DL or VU """
     sock = connect_remote()
-    #sock.sendall(b"last_seen "+choose_bd(bd)+ b" -w\n")
     sock.sendall(b"last_seen "+choose_bd(bd) +b"-w \n")
     xml = sock.recv(4096).decode('utf-8')
     
@@ -47,7 +51,14 @@ def read_last_xml(bd):
     return xml
 
 def infos_last(type_infos,sep,bd):
+    """ The function get the info in the bd
+    Params
+    type_infos -- can either be MANGA or SHOW or * 
+    sep        -- is the separtor between the name in the bd
+    bd         -- can be DL or VU 
     
+    the function return a list of list of info  """
+
     root_xml = etree.fromstring(read_last_xml(bd))
     infos = root_xml.xpath("//info")
     list_infos = []
@@ -63,7 +74,7 @@ def infos_last(type_infos,sep,bd):
     return list_infos
 
 def upd_last_manga(nom_manga,num_chap,bd):
-    
+    """ The function update a nom_manga to the num_chap in the bd  """ 
     list_infos = infos_last(type_info,".",bd)
     sock = connect_remote()
 
@@ -80,7 +91,7 @@ def upd_last_manga(nom_manga,num_chap,bd):
     sock.close()
 
 def upd_last_show(name_show,num_season,num_episode,bd):
-    
+    """ The function update a name_show to the num_season and num_episode in the bd  """ 
     list_show = infos_last("SHOW",".",bd)
     sock = connect_remote()
 
@@ -96,7 +107,7 @@ def upd_last_show(name_show,num_season,num_episode,bd):
         sock.sendall(b"last_seen "+choose_bd(bd)+b" -u "+show_info[0].encode() + b" -s "+num_season.encode() + b" -e " + num_episode.encode())
 
 def incr_last(name,bd):
-    
+    """ The function incremente the name in the bd """
     infos = infos_last("*",".",bd)
     sock = connect_remote()
 
@@ -106,7 +117,7 @@ def incr_last(name,bd):
     sock.close()
     
 def add_manga(name,chap,bd):
-    
+    """ The function add a manga name with the chap in the bd """
     sock = connect_remote()
 
     sock.sendall(b"last_seen "+choose_bd(bd) + b" -a "+name.encode()+ b" -c "+chap.encode())
@@ -114,7 +125,7 @@ def add_manga(name,chap,bd):
     sock.close()
 
 def add_show(name,season,episode,bd):
-    
+    """ The function add a show name with the season and episode in the bd """
     sock = connect_remote()
     
     sock.sendall(b"last_seen "+choose_bd(bd) + b" -a "+name.encode() + b" -s "+season.encode() + b" -e "+episode.encode())
@@ -122,7 +133,7 @@ def add_show(name,season,episode,bd):
     sock.close()
 
 def suppr_info(name,bd):
-    
+    """ The function delete the line whith name in the bd """
     list_info = infos_last("*",".",bd)
     sock = connect_remote()
 
@@ -133,9 +144,9 @@ def suppr_info(name,bd):
                  
     sock.close()    
 
-#Rajoute un zéro si le chiffre est inférieur à 10
 def format_number_zero(number_list):
-    
+    """ The function format correctly a the number in number_list and returns a str list correctly formated
+    the list number_list can contains str or int, it adds a 0 to the number lesser than 10 """
     new_list = []
     for n in number_list:
 
@@ -146,9 +157,9 @@ def format_number_zero(number_list):
 
     return new_list
 
-#Récupére le numéro de saison et d'épisode d'une regex
 def parse_regex(res_regex):
-         
+    """ The function parse the regex to get the numero of the season and the numero of the episode
+    Return a the num_season and the num_episode in a list """
     if(res_regex.group(1) != None):
         num_season_cur = res_regex.group(1)
         num_episode_cur = res_regex.group(2)
@@ -158,9 +169,11 @@ def parse_regex(res_regex):
                 
     return format_number_zero([num_season_cur,num_episode_cur])
 
-#Format le nom d'une série ou d'un manga
 def format_name(name,sep):
-    
+    """ The function format the name to be the name of the file 
+    sep  -- is separator who will be replace by a '.' 
+    Return a name separated by '.' and whith first letter of each word upper
+    """
     name_f = '.'.join(word[0].upper() + word[1:] for word in name.lower().split(sep))    
     return name_f
 
