abf789b2bf4329c3f4a6d8f9d0b1283d42840967
yosholo <mathieu.lgrand@gmail.com>
2013-03-26 11:20:48 +0100

    connect to mongoDB

---
+++ easysummary.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pymongo import Connection
+from easylast import *
+
+def connect_to_summary():
+    connect = Connection("localhost",12345)
+    db = connect["summary_mangas_shows"]
+    return  db["summary"]
+
+
+def find_summary(name):
+    """ infos is the result of the function infos_of_name """
+    
+    print(summary.find_one({"name_manga":"/"+name+"/i"}))
+
+def contrainst(infos):
+    if(is_manga(infos):
+       name = "name_manga"
+       num = "num_chap"       
+
+
+summary = connect_to_summary()
+
+
+
+print(summary.find_one())
