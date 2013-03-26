c05556f0a88a833a64f7eabe0b00ea7047b8d119
yosholo <mathieu.lgrand@gmail.com>
2013-03-26 21:54:05 +0100

    fixes #3 changes infos into dictionary

--- easylast.py
+++ easylast.py
@@ -44,18 +44,17 @@ def write_info(info,dl_or_seen,cmd="ADD"):
     info_config = read_config(path_file_info+"."+dl_or_seen)
     
     if cmd == "ADD":
-        if len(info) == 2:
-            info_config["MANGA"][info[0]] = str(info[1])
+        if is_manga(info):
+            info_config["MANGA"][info["name"]] = str(info["num"]["chap"])
         else:
-            info_config["SHOW"][info[0]] = str(info[1])+","+str(info[2])
+           # info_config["SHOW"][info["name"]] = info["num"]
+            info_config["SHOW"][info["name"]] = str(info["num"]["season"])+","+str(info["num"]["episode"])
     elif cmd == "DEL":
-        if len(info) == 2:
-            del(info_config["MANGA"][info[0]])
-        else:
-            del(info_config["SHOW"][info[0]])
+        del(info_config[info["type"]][info["name"]])
+        
 
     with open(path_file_info+"."+dl_or_seen, 'w') as configfile:
-        info_config.write(configfile)
+        info_config.write(configfile) 
 
 def read_config(path_config):
     info_config = configparser.ConfigParser()
@@ -64,147 +63,83 @@ def read_config(path_config):
     return info_config
 
 def is_manga(info):
-    if len(info) == 2:
+    if info["type"] == "MANGA":
         return True
     else:
         return False
     
 def is_show(info):
-    if len(info) == 3:
+    if info["type"] == "SHOW":
         return True
     else:
         return False
 
-def find_info(name,infos):
+def infos_last(type_infos,sep=".",dl_or_seen="VU"):
 
-    """ Return the infos which correspond to the real name in the bd """    
-    ret = None
-    for info in infos:
-        if re.search(name,info[0],re.IGNORECASE) != None :
-            ret = info
-    return ret
-
-
-
-def infos_last(type_infos,sep,dl_or_seen):
-
-    info_config = read_config(path_file_info+"."+dl_or_seen)
-    
+    info_config = read_config(path_file_info+"."+dl_or_seen)    
     ret = []
     
     for section in info_config.sections():
         
         for k in info_config[section].keys():
             if section == type_infos == "SHOW":
-                
-                num = info_config[section][k].split(',')
-                elem = [k.replace('.',sep),int(num[0]),int(num[1])]
-                ret.append(elem)
+
+                nums = info_config[section][k].split(',')
+                info = {"type":"SHOW","name" : k.replace('.',sep),"num":{"season":int(nums[0]),"episode":int(nums[1])}}
+                ret.append(info)
 
             if section == type_infos == "MANGA":
-                ret.append([k.replace('.',sep),int(info_config[section][k])])
+                info = {"type":"MANGA","name" : k.replace('.',sep),"num":{"chap":int(info_config[section][k])}}
+
+                ret.append(info)
     return ret  
-
-
-def upd_last_manga(nom_manga,num_chap,ext_config):
-    """ The function update a nom_manga to the num_chap in the config_file.ext_config """ 
-    
-    list_infos = infos_last("MANGA",".",ext_config)
-    manga_info = find_info(nom_manga,list_infos)
-    
-    if manga_info == None:
-        return False
-    
-    manga_info[1] = str(num_chap)
-    write_info(manga_info,ext_config)
-    
-    return True
-
-def upd_last_show(name_show,num_season,num_episode,ext_config):
-    """ The function update a name_show to the num_season and num_episode in the bd  """ 
-    list_show = infos_last("SHOW",".",ext_config)
-    show_info = find_info(name_show,list_show)
-
-    if show_info == None:
-        return False
-    
-    show_info[2] = str(num_episode)
-    show_info[1] = str(num_season)
-    write_info(show_info,ext_config)
-
-def incr_last(name,ext_config):
-    """ The function incremente the name in the bd """
-    
-    infos_all = infos_last("MANGA",".",ext_config) + infos_last("SHOW",".",ext_config)
-
-    infos = find_info(name,infos_all)
-
-    if len(infos) == 3:
-        infos[2]+=1
-    if len(infos) == 2:
-        infos[1]+=1
-    write_info(infos,ext_config)
-    
-def add_manga(name,chap,bd):
-    """ The function add a manga name with the chap in the bd """
-    write_info([name,chap],bd)
-    
-
-def add_show(name,season,episode,bd):
-    """ The function add a show name with the season and episode in the bd """
-    write_info([name,season,episode],bd)
-
-def suppr_info(name,bd):
-    """ The function delete the line whith name in the bd """
-    list_info = infos_last("MANGA",".",bd)+infos_last("SHOW",".",bd)
-    
-    infos = find_info(name,list_info)
-    
-    write_info(infos,bd,"DEL")
-
-def format_number_zero(number_list):
-    """ The function format correctly a the number in number_list and returns a str list correctly formated
-    the list number_list can contains str or int, it adds a 0 to the number lesser than 10 """
-    new_list = []
-    for n in number_list:
-
-        if int(n) < 10:
-            new_list.append("0"+str(int(n)))
-        else:
-            new_list.append(str(n))
-
-    return new_list
-
-def parse_regex(res_regex):
-    """ The function parse the regex to get the numero of the season and the numero of the episode
-    Return a the num_season and the num_episode in a list or the num_chap"""
-    if(res_regex.group(1) != None):
-        num_season_cur = res_regex.group(1)
-        num_episode_cur = res_regex.group(2)
-    elif res_regex.group(3) != None:
-        num_season_cur = res_regex.group(3)
-        num_episode_cur = res_regex.group(4)
-    else:
-        return format_number_zero([res_regex.group(5)])
-    
-    return format_number_zero([num_season_cur,num_episode_cur])
-
-def format_name(name,sep):
-    """ The function format the name to be the name of the file 
-    sep  -- is separator who will be replace by a '.' 
-    Return a name separated by '.' and whith first letter of each word upper
-    """
-    name_f = '.'.join(word[0].upper() + word[1:] for word in name.lower().split(sep))    
-    return name_f
 
 def infos_of_name(name,ext,DEBUG=False):
 
     for info in  infos_last("MANGA",".",ext) + infos_last("SHOW",".",ext):
-        if re.search(name,info[0],re.IGNORECASE):
+        
+        if re.search(name,info["name"],re.IGNORECASE):
             return info
     if DEBUG == True:
         print(name,"infos_of_name :",name,"Nothing to do")
     return None
+
+def upd_last(name,hash_num,ext_config):
+    info = infos_of_name(name,ext_config)
+    
+    if info == None:
+        return False
+
+    info["num"] = hash_num
+    write_info(info,ext_config)
+    
+    return True
+
+def incr_last(name,ext_config):
+    """ The function incremente the name in the config file """
+    
+    infos = infos_of_name(name,ext_config)
+
+    if is_manga(infos):
+        infos["num"]["chap"]+=1
+    if is_show(infos):
+        infos["num"]["episode"]+=1
+
+    write_info(infos,ext_config)    
+
+def add_manga(name,chap,ext_config):
+    """ The function add a manga name with the chap in the config file"""
+    write_info({"type":"MANGA","name":name,"num":hash_num},ext_config)
+  
+def add_show(name,season,episode,ext_config):
+    """ The function add a show name with the season and episode in the config file """
+    write_info({"type":"SHOW","name":name,"num":hash_num},ext_config)    
+
+def suppr_info(name,ext_config):
+    """ The function delete the line whith name in the config file """
+
+    info = infos_of_name(name,ext_config)
+    write_info(infos,ext_config,"DEL")
 
 def path_of_episode(name,season,episode,srt=False):
 
@@ -221,8 +156,7 @@ def path_of_episode(name,season,episode,srt=False):
             path_ep = path_shows+show+"/Saison."+str(season)+"/"
 
     if srt==True:
-        path_ep_srt=["",""]
-   
+        path_ep_srt=["",""]   
    
     if os.path.exists(path_ep):
         list_episode = os.listdir(path_ep)
@@ -244,6 +178,41 @@ def path_of_episode(name,season,episode,srt=False):
         return path_ep
     else:
         return path_ep_srt
+
+def parse_regex(res_regex):
+    """ The function parse the regex to get the numero of the season and the numero of the episode
+    Return a the num_season and the num_episode in a list or the num_chap"""
+    if(res_regex.group(1) != None):
+        num_season_cur = res_regex.group(1)
+        num_episode_cur = res_regex.group(2)
+    elif res_regex.group(3) != None:
+        num_season_cur = res_regex.group(3)
+        num_episode_cur = res_regex.group(4)
+    else:
+        return format_number_zero([res_regex.group(5)])
+    
+    return format_number_zero([num_season_cur,num_episode_cur])
+
+def format_number_zero(number_list):
+    """ The function format correctly a the number in number_list and returns a str list correctly formated
+    the list number_list can contains str or int, it adds a 0 to the number lesser than 10 """
+    new_list = []
+    for n in number_list:
+
+        if int(n) < 10:
+            new_list.append("0"+str(int(n)))
+        else:
+            new_list.append(str(n))
+
+    return new_list
+
+def format_name(name,sep):
+    """ The function format the name to be the name of the file 
+    sep  -- is separator who will be replace by a '.' 
+    Return a name separated by '.' and whith first letter of each word upper
+    """
+    name_f = '.'.join(word[0].upper() + word[1:] for word in name.lower().split(sep))    
+    return name_f
 
 def format_SXXEXX(num_season,num_episode):
     num = format_number_zero([num_season,num_episode])
@@ -284,4 +253,4 @@ def read_inform(path_fifo="/home/yosholo/.config/utils/.inform_fifo"):
     os.close(fifo)
     return message.decode("utf-8")
 
-__version__ = '0.2'
+__version__ = '0.3'
